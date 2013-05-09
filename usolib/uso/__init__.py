219cae6de1da3479893bf090b3051b293d351e7f
Vitor <vitor@itangast.(none)>
2013-05-09 19:13:01 +0200

    Add new uso.

--- usolib/uso/__init__.py
+++ usolib/uso/__init__.py
@@ -1,5 +1,12 @@
 import fst
 
+# trivial uso with 1...1 as the sink
+table = {(0, "0"): (0, "-"),
+         (0, "1"): (0, "+")}
+ascending = fst.SimpleFST(table)
+
+
+# slow uso that is not matousek
 table = {(0, "0"): (1, "+"),
          (0, "1"): (2, "-"),
          (1, "0"): (2, "-"),
@@ -8,9 +15,23 @@ table = {(0, "0"): (1, "+"),
          (2, "1"): (1, "-")}
 bad = fst.SimpleFST(table)
 
-table = {(0, "0"): (0, "-"),
-         (0, "1"): (0, "+")}
-ascending = fst.SimpleFST(table)
+
+# slow uso that is matousek, with a matrix of the form:
+#    1
+#    1 1
+#    0 1 1
+#    1 0 1 1
+#    1 1 0 1 1
+#    0 1 1 0 1 1
+table = {("start", "0"): ("start", "+"),
+         ("start", "1"): ("e2", "-"),
+         ("e1", "0"): ("e2", "-"),
+         ("e1", "1"): ("start", "+"),
+         ("e2", "0"): ("o1", "-"),
+         ("e2", "1"): ("e1", "+"),
+         ("o1", "0"): ("e1", "+"),
+         ("o1", "1"): ("o1", "-")}
+test = fst.SimpleFST(table, start_state="start")
 
 del table
 
