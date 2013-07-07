aede4055a66b723f0902919c3e3a77b24ff2a230
Vitor <vitor@itangast.(none)>
2013-07-07 08:39:25 +0200

    Use new pmap API.

--- calculate_runtime_analytic.py
+++ calculate_runtime_analytic.py
@@ -5,7 +5,7 @@ import usolib
 from usolib.helpers import *
 
 
-def main((start, step, n)):
+def main(n, step, start):
     uso = usolib.uso.bad()
     vertices = generate_vertices(n, start, step)
     return usolib.randomfacet.randomfacet_analytic(uso, n, vertices)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print " n\ttime\tavg"
     for n in range(1, 11):
         t = time.time()
-        lst = pmap(main, ((i, n_processes, n) for i in range(n_processes)), n_processes)
+        lst = pmap(main, range(n_processes), n_processes, args=(n, n_processes))
         res = sum(lst) / float(factorial(n) * 2**n)
         #res = max(lst) / float(factorial(n))
         print "%2d\t%.2fs\t%.4f" %(n, time.time() - t, res)
