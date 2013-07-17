c949a1c2fa78567eaf60239fbc73eba7a44f7940
Vitor <vitor@itangast.(none)>
2013-07-17 12:46:37 +0200

    More work on runtime of subcubes.

--- runtime_of_subcubes.py
+++ runtime_of_subcubes.py
@@ -41,16 +41,34 @@ def map_vals(d, f):
     return dict((k, f(v)) for (k,v) in d.iteritems())
 
 
+def map_kv(d, f):
+    """
+    Apply f to each key value pair in the dict to produce a new value.
+    """
+    return dict((k, f(k, v)) for (k,v) in d.iteritems())
+
+
+def runtime_fm_raw_runtimes(subcube, runtimes):
+    """
+    take list of raw runtimes (i.e. sum over all executions for each vertex of subcube)
+    and turn it into global runtime.
+    """
+    n = get_dimension(subcube)
+    assert 2**n == len(runtimes)
+    return sum(runtimes) / float(2**n * factorial(n))
+
 
 def main(dim, d):
+    print dim
     d = accumulate_vals(d, lambda (vertex, subcube): subcube)
-    d = map_vals(d, sum)
+    d = map_kv(d, runtime_fm_raw_runtimes)
     c = defaultdict(int)
     for v in d.itervalues():
         c[v] += 1
 
     for i in sorted(c.items(), key = lambda (runtime, count): count):
         print i
+    print
 
 
 if __name__ == "__main__":
@@ -58,19 +76,15 @@ if __name__ == "__main__":
 
     # accumulate data into cache
     cache = {}
-    cache = CountingDict()
     uso = usolib.uso.bad4state()
     usolib.randomfacet.randomfacet_analytic(uso, N, cache=cache)
 
     #  
-    by_dim = partition(cache.counter, lambda (vertex, subcube), _: get_dimension(subcube))
+    by_dim = partition(cache, lambda (vertex, subcube), _: get_dimension(subcube))
     by_dim = sorted(by_dim.iteritems())
 
     for dim, d in by_dim:
-        print dim
-        print len(d)
         main(dim, d)
-        print
 
 
 
