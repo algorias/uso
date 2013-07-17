5b958426b83be176fe9d5c82f1fab6965ce0854f
Vitor <vitor@itangast.(none)>
2013-07-17 10:48:58 +0200

    Updated runtime_of_subcubes script.

--- runtime_of_subcubes.py
+++ runtime_of_subcubes.py
@@ -5,16 +5,6 @@ import usolib
 from usolib.helpers import *
 
 
-def get_execution_data(n, uso, cache=None):
-    """ execute randomfacet and return cache data"""
-    if cache is None:
-        cache = {}
-    # replace cache with own cache instance
-    #usolib.randomfacet.cache = cache
-    usolib.randomfacet.randomfacet_analytic(uso, n, cache)
-    return cache
-
-
 def partition(d, f):
     """ 
     Partition entries in dict d by function f, which takes a key and a value as input.
@@ -37,7 +27,7 @@ def accumulate_vals(d, f=lambda x: x):
 
 def accumulate_keys(d, f=lambda x: x):
     """
-    Accumulate values of dict by keys (or an equivalence relation f on the keys).
+    Accumulate keys of dict by keys (or an equivalence relation f on the keys).
     """
     acc = defaultdict(list)
     for (k,v) in d.iteritems():
@@ -48,42 +38,39 @@ def map_vals(d, f):
     """
     Apply f to each value in the dict.
     """
-    res = {}
-    for (k,v) in d.iteritems():
-        res[k] = f(v)
-    return res
+    return dict((k, f(v)) for (k,v) in d.iteritems())
 
 
-def main(dim, d, cache):
-    # summarise argument pairs by the number of times they were called in total
-    by_ncalls = accumulate_keys(d)
 
-    # it's also possible to accumulate results by subcube regardless of the vertex we're at, but the
-    # results are not so clear.
-    #d = accumulate_by_key(d, lambda (vertex, cube): cube)
-    #d = map_vals(d, sum)
-    #by_ncalls = accumulate_by_val(d)
+def main(dim, d):
+    d = accumulate_vals(d, lambda (vertex, subcube): subcube)
+    d = map_vals(d, sum)
+    c = defaultdict(int)
+    for v in d.itervalues():
+        c[v] += 1
 
-    for (k,v) in by_ncalls.items():
-        print "%s, %s" %(k, len(v))
-        pprint.pprint(v)
+    for i in sorted(c.items(), key = lambda (runtime, count): count):
+        print i
 
 
 if __name__ == "__main__":
     N = 5
 
     # accumulate data into cache
+    cache = {}
     cache = CountingDict()
-    uso = usolib.uso.bad()
-    get_execution_data(N, uso, cache)
+    uso = usolib.uso.bad4state()
+    usolib.randomfacet.randomfacet_analytic(uso, N, cache=cache)
 
     #  
     by_dim = partition(cache.counter, lambda (vertex, subcube), _: get_dimension(subcube))
     by_dim = sorted(by_dim.iteritems())
 
-    # by_dim is list of (dimension, dict) tuples.
-    # dicts have same format as get_execution_data
+    for dim, d in by_dim:
+        print dim
+        print len(d)
+        main(dim, d)
+        print
 
-    dim, d = by_dim[N-1]
-    main(dim, d, cache)
+
 
