2fd455cb8e90367b299beeb22579133e885c37e9
Vitor <vitor@itangast.(none)>
2013-07-28 00:04:45 +0200

    Modified script to use new analytic algo.

--- calculate_runtime_analytic.py
+++ calculate_runtime_analytic.py
@@ -3,20 +3,15 @@ import time
 import usolib
 from usolib.helpers import *
 
-
-def main(n, step, start):
-    uso = usolib.uso.bad()
-    vertices = generate_vertices(n, start, step)
-    return usolib.randomfacet.randomfacet_analytic(uso, n, vertices)
+def main(n):
+    uso = usolib.uso.bad4state()
+    return usolib.randomfacet.randomfacet_analytic_new(uso, n)
 
 
 if __name__ == "__main__":
-    n_processes = 6
     print " n\ttime\tavg"
     for n in range(1, 11):
         t = time.time()
-        lst = pmap(main, range(n_processes), n_processes, args=(n, n_processes))
-        res = sum(lst) / float(factorial(n) * 2**n)
-        #res = max(lst) / float(factorial(n))
+        res = main(n) / float(factorial(n) * 2**n)
         print "%2d\t%.2fs\t%.4f" %(n, time.time() - t, res)
 
